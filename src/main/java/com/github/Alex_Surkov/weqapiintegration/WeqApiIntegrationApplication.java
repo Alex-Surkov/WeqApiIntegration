2fdcd840a541a8efb2b073839a7ae05e098c0a57
alexandr <nordgeocompany@gmail.com>
2025-08-23 12:03:31 +0300

    Initial

---
+++ src/main/java/com/github/Alex_Surkov/weqapiintegration/WeqApiIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.github.Alex_Surkov.weqapiintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeqApiIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeqApiIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/Alex_Surkov/weqapiintegration/WeqApiIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.Alex_Surkov.weqapiintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeqApiIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
